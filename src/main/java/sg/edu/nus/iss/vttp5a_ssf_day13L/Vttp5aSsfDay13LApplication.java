66584424d47ccbfade8e638715b5fc3939cf3988
maziyyahjaf <mazi3ah@gmail.com>
2024-11-20 10:49:15 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/vttp5a_ssf_day13L/Vttp5aSsfDay13LApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a_ssf_day13L;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5aSsfDay13LApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5aSsfDay13LApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/vttp5a_ssf_day13L/Vttp5aSsfDay13LApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a_ssf_day13L;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aSsfDay13LApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
